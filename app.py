6ee89582f826ec91926c95c6e5b593927728c40a
Iskandar Kurbonov <iskandar.kurbonov@onlyoffice.com>
2023-06-23 23:15:45 +0300

    Some refactoring

--- app.py
+++ app.py
@@ -1,89 +1,37 @@
-from flask import Flask, render_template, request, redirect, url_for
-from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
-from werkzeug.security import generate_password_hash, check_password_hash
+from flask import Flask, render_template, redirect, request, url_for
+import requests
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret_key'
-
-# Создаем соединение с БД
-engine = create_engine('sqlite:///users.db')
-Session = sessionmaker(bind=engine)
-session = Session()
-
-# Создаем базу данных
-Base = declarative_base()
-
-
-class User(Base):
-    __tablename__ = 'users'
-    id = Column(Integer, primary_key=True)
-    username = Column(String(50), unique=True)
-    password = Column(String(100))
-
-    def __repr__(self):
-        return f'<User {self.username}>'
-
-# Создаем таблицы в БД
-Base.metadata.create_all(engine)
-
-
-class Main:
-    def __init__(self):
-        self.weather = "Weather"
-        self.auth = "Authorization"
-        self.info = "Info about service"
-        self.contacts = "Contacts"
 
 
 @app.route('/')
 def index():
-    main = Main()
-    username = request.cookies.get('username')
-    return render_template('index.html', main=main, username=username)
+    return render_template('index.html')
 
 
-@app.route('/weather')
+@app.route('/get_weather', methods=['POST'])
+def get_weather():
+    city = request.form['city']
+    api_key = 'b41f842f9800a4d28dbe5d5bbea18a3e'
+    url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}'
+    response = requests.get(url)
+    data = response.json()
+    weather_description = data['weather'][0]['description']
+    temperature = round(data['main']['temp'] - 273.15, 1)
+    wind_speed = data['wind']['speed']
+    return render_template('get_weather.html', city=city, weather=weather_description, temperature=temperature, wind_speed=wind_speed)
+
+
+@app.route('/weather', methods=['GET', 'POST'])
 def weather():
+    if request.method == 'POST':
+        return redirect(url_for('get_weather'))
     return render_template('weather.html')
 
 
 @app.route('/info')
 def info():
     return render_template('info.html')
-
-
-@app.route('/contacts')
-def contacts():
-    return render_template('contacts.html')
-
-
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    if request.method == 'POST':
-        username = request.form.get('username')
-        password = request.form.get('password')
-        confirm_password = request.form.get('confirm_password')
-
-        # Проверяем совпадение паролей
-        if password != confirm_password:
-            return render_template('register.html', error='Passwords do not match')
-
-        # Проверяем, что пользователь с таким именем не существует
-        user = session.query(User).filter_by(username=username).first()
-        if user:
-            return render_template('register.html', error='User already exists')
-
-        # Создаем нового пользователя
-        hashed_password = generate_password_hash(password)
-        new_user = User(username=username, password=hashed_password)
-        session.add(new_user)
-        session.commit()
-
-        return redirect(url_for('index'))
-
-    return render_template('register.html')
 
 
 if __name__ == '__main__':
